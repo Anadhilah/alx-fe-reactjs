290f8d1f60669e4d6ef25dd259998284766affe3
Anastasia Andoh <142629892+Anadhilah@users.noreply.github.com>
2025-02-14 12:25:31 +0000

    Added header, main content and footer

---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer (){
+    return(
+            <div>
+                    <footer>
+                      <p>© 2023 City Lovers</p>
+                    </footer>
+            </div>
+
+
+    );
+
+
+
+}
+
+export default Footer;
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,19 @@
+function MainContent(){
+        return(
+                <div>
+                        <main>
+                            <p>I love to visit New York, Paris, and Tokyo.</p>
+                        </main>
+
+                </div>
+ 
+
+
+
+        );
+
+
+
+}
+
+export  default MainContent;
