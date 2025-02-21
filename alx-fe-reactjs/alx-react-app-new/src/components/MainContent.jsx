7f1906003ab5b4589cb1dd1bb0166c80dff23c35
Anastasia Andoh <142629892+Anadhilah@users.noreply.github.com>
2025-02-21 10:13:02 +0000

    new commit

---
+++ alx-fe-reactjs/alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,19 @@
+function MainContent(){
+        return(
+                <div>
+                        <main>
+                            <p style={{ color: 'white', textAlign: 'center' }}>I love to visit New York, Paris, and Tokyo.</p>
+                        </main>
+
+                </div>
+ 
+
+
+
+        );
+
+
+
+}
+
+export  default MainContent;
