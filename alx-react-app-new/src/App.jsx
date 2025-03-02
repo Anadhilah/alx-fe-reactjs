e97a80e960eb4ae1f5115baac8f892a7defbabc5
Anastasia Andoh <142629892+Anadhilah@users.noreply.github.com>
2025-03-02 09:18:54 +0000

    Adding counter

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,17 +7,19 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div> 
-         <Header/>
+         <Counter/>
+         {/* <Header/>
          <MainContent/>
         
          <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-         <Footer />
+         <Footer /> */}
 
     </div>
    
