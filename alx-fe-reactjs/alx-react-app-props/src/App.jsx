be4f52ee4b0adaccfb227af7e32cc518850438d8
Anastasia Andoh <142629892+Anadhilah@users.noreply.github.com>
2025-03-02 09:56:05 +0000

    updated work

--- alx-fe-reactjs/alx-react-app-props/src/App.jsx
+++ alx-fe-reactjs/alx-react-app-props/src/App.jsx
@@ -1,9 +1,16 @@
+
+import React from 'react';
 import ProfilePage from './components/ProfilePage';
+import UserContext from './components/UserContext';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return <ProfilePage userData={userData} />;
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
-export default App
+export default App;
