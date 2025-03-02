5dd589ffc8ddc9c21c5479545891b8477a44dd2c
Anastasia Andoh <142629892+Anadhilah@users.noreply.github.com>
2025-03-02 09:45:57 +0000

    working with props

---
+++ alx-fe-reactjs/alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer (){
+    return(
+            <div>
+                    <footer>
+                      <p style={{ color: 'white', textAlign: 'center' }}>© 2023 City Lovers</p>
+                    </footer>
+            </div>
+
+
+    );
+
+
+
+}
+
+export default Footer;
